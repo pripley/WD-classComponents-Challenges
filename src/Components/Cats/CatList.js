dda0912d40db6b82f2140c1405e1a154d0bda80d
Paul Ripley <paulripley@hotmail.com>
2021-10-05 21:40:59 -0400

    cats challenge update

--- src/Components/Cats/CatList.js
+++ src/Components/Cats/CatList.js
@@ -2,10 +2,10 @@ import React, { Component } from "react";
 
 class CatList extends Component {
   render() {
-    const cats = () => this.props.breeds.map((cat) => <li>{cat}</li>);
+    const cats = () => this.props.cats.map((cat, index) => <li key={cat}>{cat}</li>);
     return (
       <div>
-        <ul>{cats}</ul>
+        <ul>{cats()}</ul>
       </div>
     );
   }
